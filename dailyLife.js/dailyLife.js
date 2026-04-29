4ffa109064c8784a36af6e6c04abecf9e4705520
REMMYSDDD <remmyntwali@gmail.com>
2026-04-29 17:51:17 +0300

    Add files via upload

---
+++ dailyLife.js/dailyLife.js
@@ -0,0 +1,26 @@
+// Part 1: Variables
+let name = "Remmy";
+let age = 17;
+let favoriteFood = "rice";
+let studyHoursPerDay = 3;
+
+// Bonus variable
+let isStudent = true;
+
+// Part 2: Calculations
+let weeklyStudyHours = studyHoursPerDay * 7;
+let ageInMonths = age * 12;
+
+// Bonus calculation (dog years)
+let dogYears = age * 7;
+
+// Part 3: Output
+console.log("Hello, my name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I love eating " + favoriteFood + ".");
+console.log("I study " + studyHoursPerDay + " hours per day, which is " + weeklyStudyHours + " hours per week.");
+console.log("I am " + ageInMonths + " months old.");
+
+// Bonus output
+console.log("In dog years, I am " + dogYears + " years old.");
+console.log("Am I a student? " + isStudent);
